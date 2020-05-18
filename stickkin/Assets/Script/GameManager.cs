7dd8306ce1d2f36337f440f0cf9d2d60ecab7e7a
姜赫赫 <jianghehe@jianghehedeMBP.lan>
2020-05-18 23:42:30 +0800

    unity Study step1

---
+++ stickkin/Assets/Script/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform startPoint;
+    private Transform spawnPoint;
+    
+    public GameObject pinPrefab;
+    void Start()
+    {
+        startPoint = GameObject.Find("StartPoint").transform;
+        spawnPoint = GameObject.Find("SpawnPoint").transform;
+        SpawnPin();
+        //Destory(this);
+    }
+    void SpawnPin()
+    {
+        //Instantiate(pinPrefab,this);
+        GameObject wee = GameObject.Instantiate(pinPrefab,spawnPoint.position,pinPrefab.transform.rotation);
+        wee.transform.parent = this.transform;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ stickkin/Assets/Script/Pin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool isFly = false;
+    private bool isReach = false;
+    private Transform StartPoint;
+    void Start()
+    {
+        StartPoint = GameObject.Find("StartPoint").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
