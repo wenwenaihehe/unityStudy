d9621b524b43f239dfc4c77507811d7eda0083c6
姜赫赫 <jianghehe@jianghehedeMBP.lan>
2020-05-20 00:13:33 +0800

    jfcz

--- stickkin/Assets/Script/GameManager.cs
+++ stickkin/Assets/Script/GameManager.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     private Transform startPoint;
     private Transform spawnPoint;
     
+
     public GameObject pinPrefab;
+    private Pin CurrentPin;
+    private bool isover = false;
+    public Text score;
+    private int sc = 0;
     void Start()
     {
+        score.text = "0";
         startPoint = GameObject.Find("StartPoint").transform;
         spawnPoint = GameObject.Find("SpawnPoint").transform;
         SpawnPin();
@@ -19,12 +25,24 @@ public class GameManager : MonoBehaviour
     void SpawnPin()
     {
         //Instantiate(pinPrefab,this);
-        GameObject wee = GameObject.Instantiate(pinPrefab,spawnPoint.position,pinPrefab.transform.rotation);
-        wee.transform.parent = this.transform;
+        CurrentPin = GameObject.Instantiate(pinPrefab,spawnPoint.position,pinPrefab.transform.rotation).GetComponent<Pin>();
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if(isover) return ;
+        if(Input.GetMouseButtonDown(0))
+        {
+            sc++;
+            score.text = sc.ToString();
+            CurrentPin.StartFly();
+            SpawnPin();
+        }
+    }
+    public void GameOver()
+    {
+        if(isover) return ;
+        GameObject.Find("Circle").GetComponent<RoateSelf>().enabled = false;
+        isover = true;
     }
 }
--- stickkin/Assets/Script/Pin.cs
+++ stickkin/Assets/Script/Pin.cs
@@ -8,14 +8,50 @@ public class Pin : MonoBehaviour
     private bool isFly = false;
     private bool isReach = false;
     private Transform StartPoint;
+    public float speed = 5;
+    private Transform circle;
+
+    private Vector3 targetCirclePos;
     void Start()
     {
         StartPoint = GameObject.Find("StartPoint").transform;
+        circle = GameObject.Find("Circle").transform;
+        targetCirclePos = circle.position;
+        targetCirclePos.y = targetCirclePos.y - 1.4f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isFly == false )
+        {
+            if(isReach == false)
+            {
+                transform.position = Vector2.MoveTowards(transform.position,StartPoint.position,speed*Time.deltaTime);
+                if(Vector2.Distance(transform.position,StartPoint.position) < 0.05f)
+                {
+                    isReach = true;
+                }
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position,targetCirclePos,speed * Time.deltaTime);
+            if(Vector2.Distance(transform.position,targetCirclePos) < 0.05f)
+            {
+                transform.position = targetCirclePos;
+                transform.parent = circle;
+                isFly = false;
+            }
+        }
+    }
+    public void StartFly()
+    {
+        isFly = true;
+        isReach = true;
     }
 }
