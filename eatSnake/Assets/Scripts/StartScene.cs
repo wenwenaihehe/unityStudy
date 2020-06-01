6cab32e62a9ad7e209f7f1678b6dedd9f2640714
jianghe <he.jiang@microfun.com>
2020-06-01 23:42:29 +0800

    unity

---
+++ eatSnake/Assets/Scripts/StartScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class StartScene : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod]
+    static void Initialize()
+    {
+        string startSceneName = "LobbyScene";
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name.Equals(startSceneName))
+        {
+            return;
+        }
+        SceneManager.LoadScene(startSceneName);
+    }
+}
