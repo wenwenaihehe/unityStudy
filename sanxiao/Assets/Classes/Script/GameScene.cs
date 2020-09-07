227ad8136ccdc0b548ff6130c6ae677c2005061f
jianghe <he.jiang@microfun.com>
2020-09-07 23:04:56 +0800

    unity update sanxiao

---
+++ sanxiao/Assets/Classes/Script/GameScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int nHeight = 9;
+    public int nWidth = 9;
+    private GameObject pFruitBack;
+    void Start()
+    {
+        pFruitBack = GameObject.Find("Back");
+        GameObject pFruit = (GameObject)Resources.Load("Prefab/Image");
+        GameObject pRet = Instantiate(pFruit);
+        pRet.transform.SetParent(pFruitBack.transform);
+        pRet.transform.localPosition = Vector3.zero;
+        pRet.transform.localScale = Vector3.one;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ sanxiao/Assets/Classes/Script/LobbyScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LobbyScene : MonoBehaviour
+{
+    private GameObject m_pStartButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_pStartButton = GameObject.Find("button");
+        m_pStartButton.GetComponent<Button>().onClick.AddListener(onBtnStartGame);
+    }
+
+    void onBtnStartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+        //int t = 1;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
