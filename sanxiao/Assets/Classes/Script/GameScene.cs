fa2b42f8bcc39986284886adaadb53560c4885cf
jianghe <he.jiang@microfun.com>
2020-09-09 00:06:58 +0800

    update

--- sanxiao/Assets/Classes/Script/GameScene.cs
+++ sanxiao/Assets/Classes/Script/GameScene.cs
@@ -7,16 +7,22 @@ public class GameScene : MonoBehaviour
     // Start is called before the first frame update
     public int nHeight = 9;
     public int nWidth = 9;
+    public int beginX = 70;
+    public int beginY = 70;
+    public int SpacingX = 5; //x的间隔
+    public int SpacingY = 5; //y的间隔
+
     private GameObject pFruitBack;
+    private GameObject[,] m_pTile;
+    private GameObject[,] m_pItem;
+    private Vector3 startPoint;
     void Start()
     {
         pFruitBack = GameObject.Find("Back");
-        GameObject pFruit = (GameObject)Resources.Load("Prefab/Image");
-        GameObject pRet = Instantiate(pFruit);
-        pRet.transform.SetParent(pFruitBack.transform);
-        pRet.transform.localPosition = Vector3.zero;
-        pRet.transform.localScale = Vector3.one;
-
+        Rect backSize = pFruitBack.GetComponent<RectTransform>().rect;
+        startPoint = new Vector3(backSize.width / 2 * -1, backSize.height / 2 * -1, 0);
+        initTile();
+        initItem();
     }
 
     // Update is called once per frame
@@ -24,4 +30,58 @@ public class GameScene : MonoBehaviour
     {
         
     }
+    void initTile()
+    {
+        m_pTile = new GameObject[nWidth, nHeight];
+
+        for (int x = 0; x < nWidth; x++)
+        {
+            for (int y = 0; y < nHeight; y++)
+            {
+                GameObject pTile = getTile();
+                pTile.transform.SetParent(pFruitBack.transform);
+                float nWidth = pTile.GetComponent<RectTransform>().rect.width;
+                float nHeight = pTile.GetComponent<RectTransform>().rect.height;
+
+              
+                pTile.transform.localScale = Vector3.one;
+                Vector3 pos = new Vector3(beginX + x * (nWidth + SpacingX), beginY + y * (nHeight + SpacingY), 0);
+                pTile.transform.localPosition = pos + startPoint;
+
+                m_pTile[x, y] = pTile;
+            }
+        }
+    }
+    GameObject getTile()
+    {
+        GameObject pTile = (GameObject)Resources.Load("Prefab/Tile");
+        GameObject pRet = Instantiate(pTile);
+        pRet.transform.localPosition = Vector3.zero;
+        pRet.transform.localScale = Vector3.one;
+        return pRet;
+    }
+    void initItem()
+    {
+        m_pItem = new GameObject[nWidth, nHeight];
+
+        for (int x = 0; x < nWidth; x++)
+        {
+            for (int y = 0; y < nHeight; y++)
+            {
+                GameObject pTile = m_pTile[x, y];
+                GameObject pItem = getItem();
+                pItem.transform.SetParent(pTile.transform);
+                pItem.transform.localScale = Vector3.one;
+                pItem.transform.localPosition = Vector3.zero;
+            }
+        }
+    }
+    GameObject getItem()
+    {
+        GameObject pItem = (GameObject)Resources.Load("Prefab/Image");
+        GameObject pRet = Instantiate(pItem);
+        pRet.transform.localPosition = Vector3.zero;
+        return pRet;
+    }
+
 }
--- sanxiao/Assets/Classes/Script/fruit.cs
+++ sanxiao/Assets/Classes/Script/fruit.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class fruit : MonoBehaviour
 {
     // Start is called before the first frame update
+    private int m_nColor;
+
     void Start()
     {
-        
+        m_nColor = Random.Range(1 , 11);
+
+        string address = "xiaoImage/star";
+        address = address + m_nColor.ToString();
+        Sprite Tb3 = (Sprite)Resources.Load(address, typeof(Sprite)) as Sprite;
+        gameObject.GetComponent<Image>().sprite = Tb3;
+
     }
 
     // Update is called once per frame
