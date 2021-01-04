f3baafd1a60345e834028a56d4710dcc7ca44a64
jianghe <he.jiang@microfun.com>
2021-01-04 22:06:35 +0800

    [update

---
+++ snake/Assets/Script/Classes/function/GameTouch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+public class GameTouch
+{
+    public static bool onTouchBegan()
+    {
+        if (Input.GetKeyDown(KeyCode.KeyCode.Mouse0))
+        {
+            return true;
+        }
+        return false;
+    }
+}
